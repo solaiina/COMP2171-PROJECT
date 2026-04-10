d3cac5c6a3f77197689a255da929e1fdb378551c
bwoyjaedonb <89319773+bwoyjaedonb@users.noreply.github.com>
2026-04-10 11:19:20 -0500

    Delete CALL - System - Copy/CALL - System - Copy/CALL - System - Copy/backend/backend/src directory

--- CALL - System - Copy/CALL - System - Copy/CALL - System - Copy/backend/backend/src/main/java/backend/repository/ServiceItemRepository.java
+++
@@ -1,7 +0,0 @@
-package backend.repository;
-
-import backend.model.ServiceItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceItemRepository extends JpaRepository<ServiceItem, Long> {
-}
